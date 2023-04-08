732967a12e6e463e9f7b75d7cac48ca13774bb17
Ashikuzzaman Abir <ashikuzzamanabir@outlook.com>
2023-04-09 02:55:41 +0600

    project initialization

---
+++ server/app.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+
+
+const app = express();
+const PORT= process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello I'm fahim a common dork")
+})
+
+
+app.listen(PORT, () => {
+  console.log(`server running @ ${PORT}`)
+});
